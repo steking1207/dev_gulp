3b0bff650e009ae0f6158175184ebc65f7c645e9
steking1207 <steking1207@gmail.com>
2022-04-21 08:16:06 +0800

    include

--- src/js/web/include.js
+++ src/js/web/include.js
@@ -3,7 +3,6 @@ $("#common-header").load("module/common-header.html");
 $("#common-links").load("module/common-links.html");
 $("#common-footer").load("module/common-footer.html");
 $("#nav-mobile").load("module/nav-mobile.html");
-
 //Modal
 $("#modal-cart").load("module/modal-cart.html");
 $("#modal-message").load("module/modal-message.html");
